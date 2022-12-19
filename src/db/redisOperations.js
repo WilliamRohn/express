9060ab0bcc6295898612cee87b5e28e9084efa47
wangruohan <wangruohan@chinadaas.com>
2022-12-19 17:05:33 +0800

    验证redsi密码

--- src/db/redisOperations.js
+++ src/db/redisOperations.js
@@ -5,13 +5,19 @@ exports.init = function(){
     // redis数据库,默认监听8808端口,'127.0.0.1'为本地ip(默认不需要修改)
     const redisClient  = redis.createClient('8808', '127.0.0.1');
 
-    // redis 链接错误
-    redisClient.on("ready", function(error) {
-        console.error('redis-connect is ready:');
+    // redis验证
+    redisClient.auth('root', (err, res)=>{
+        if (err&&!res) _console.pink('redis验证异常:'+err)
+    })
+
+    // redis 连接错误
+    redisClient.on("error", (err)=>{
+        _console.pink('redis连接异常:'+err);
     });
-    // redis 链接错误
-    redisClient.on("error", function(error) {
-        console.error('redis-connect is error:', error);
+
+    // redis 连接成功
+    redisClient.on("connect", ()=>{
+        _console.info('redis链接成功------------');
     });
 
     return redisClient;
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -19,31 +19,34 @@ function updateUser(id, user) {
 
 /* redisSet 往redis中存数据,expire为过期时间,单位为秒 */
 function redisSet(_key, value, expire) {
-    return new Promise((resolve, reject)=>{
+    return new Promise(async (resolve, reject)=>{
         /* redis操作的回调函数第一个参数默认是异常报错,所以一定要cb做异常处理 */
-        redisClient().set(_key,value, (err,res)=>{
-            if (err&&res) {
-                reject(null)
-            }
-            if (!isNaN(expire) && expire > 0) {
-                redisClient().expire(_key, parseInt(expire));
-                _console.info(`redis-set ${_key}写入成功,${expire}秒后过期`);
-            }
-            resolve(res)
-        })
+        try {
+            redisClient().set(_key,value, (err,res)=>{
+                if (err&&res) resolve(null);
+                if (!isNaN(expire) && expire > 0) {
+                    redisClient().expire(_key, parseInt(expire));
+                    _console.info(`redis-set ${_key}写入成功,${expire}秒后过期`);
+                }
+                resolve(res)
+            })
+        } catch (error) {
+            resolve(null)
+        }
     })
 }
 
 function redisGet(_key) {
     return new Promise((resolve, reject)=>{
         /* redis操作的回调函数第一个参数默认是异常报错,所以一定要cb做异常处理 */
-        redisClient().get(_key, (err,res)=>{
-            _console.info(`redis-get ${_key}查询成功,查询结果:${err+','+res}`);
-            if (err&&res) {
-                reject(null)
-            }
-            resolve(res)
-        })
+        try {
+            redisClient().get(_key, (err,res)=>{
+                _console.info(`redis-get ${_key}查询成功,查询结果:${err+','+res}`);
+                if (err&&res) resolve(null); else resolve(res)
+            })
+        } catch (error) {
+            resolve(null)
+        }
     });
 }
 
